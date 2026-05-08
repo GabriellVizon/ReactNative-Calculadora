b62ebe6dec19b72ab3b4c8aff012153d22df1482
GabriellVizon <gabrielfelipevizon2020@gmail.com>
2026-05-08 20:37:13 -0300

    completo

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StyleSheet, View } from 'react-native';
-import Botao from './src/components/Botao';
-import Visor from './src/components/Visor';
-import { useState } from 'react';
+import {  StyleSheet, View } from 'react-native'
+import Botao from './src/components/Botao'
+import Visor from './src/components/Visor'
+import { useState } from 'react'
 
 const estadoInicial = {
     valorVisor: '0',
@@ -13,72 +13,102 @@ const estadoInicial = {
 
 export default function App() {
 
-  const[estado, setEstado] = useState({...estadoInicial})
+    const [estado, setEstado] = useState({...estadoInicial})
 
-  const adicionarDigito = n => {
-    setEstado(prev => {
-        const limparVisor = prev.valorVisor == '0' || prev.limparVisor
+    const adicionarDigito = n => {
+        setEstado(prev => {
+           const limparVisor = prev.valorVisor == '0' || prev.limparVisor
+           
+           if(n == '.' && !limparVisor && prev.valorVisor.includes('.')) {
+                return prev
+           }
 
-        if(n == '.' && !limparVisor && prev.valorVisor.includes
-        ('.')) {
-          return prev
-        }
+           const valorAtual = limparVisor ? '' : prev.valorVisor
+           const valorVisor = valorAtual + n
 
-        const valorAtual = limparVisor ? '': prev.valorVisor
-        const valorVisor = valorAtual + n
-        
-        let valores = prev.valores
-        if(n !== '.'){
-            const novoValor = parseFloat(valorVisor)
-            valores = [...prev.valores]
-            valores[prev.valorCorrente] = novoValor
-        }
-        return {...prev, valorVisor, limparVisor: false,
-          valores}
-    })
-  }
-  return (
-    <View style={styles.container}>
-      <View style={styles.Visor}>
-          <Visor valorVisor={estado.valorVisor}/>
-      </View>
-      <View style={styles.botao}>
-        <Botao label="CE" triplo/>
-        <Botao label="/" operacao/>
+           let valores = prev.valores
+           if(n !== '.'){
+                const novoValor = parseFloat(valorVisor)
+                valores = [...prev.valores]
+                valores[prev.valorCorrente] = novoValor
+           }
+           return {...prev, valorVisor, limparVisor: false, valores}
+        })
+    }
 
-        <Botao label="7" onClick={adicionarDigito}/>
-        <Botao label="8" onClick={adicionarDigito}/>
-        <Botao label="9" onClick={adicionarDigito}/>
-        <Botao label="x" operacao/>
+    const setOperacao = operacao => {
+        setEstado(prev => {
+            if(estado.valorCorrente == 0){
+                return {...prev, operacao, valorCorrente: 1, limparVisor: true}
 
-        <Botao label="4" onClick={adicionarDigito}/>
-        <Botao label="5" onClick={adicionarDigito}/>
-        <Botao label="6" onClick={adicionarDigito}/>
-        <Botao label="-" operacao/>
+            } else {
+                const igual = operacao == '='
+                const valores = [...estado.valores]
 
-        <Botao label="3" onClick={adicionarDigito}/>
-        <Botao label="2" onClick={adicionarDigito}/>
-        <Botao label="1" onClick={adicionarDigito}/>
-        <Botao label="+" operacao/>
+                try {
+                    valores[0] = eval(`${valores[0]} ${estado.operacao} ${valores[1]}`)
+                } catch (error) {
+                    valores[0] = estado.valores[0]
+                }
+                return { 
+                  ...prev,
+                  valorVisor: `${valores[0]}`,
+                  operacao: igual ? null : operacao,
+                  valorCorrente: igual ? 0 : 1,
+                  limparVisor: true, 
+                  valores
+                }
+            }
+        })
+    }
 
-        <Botao label="0" duplo onClick={adicionarDigito}/>
-        <Botao label="."/>
-        <Botao label="=" operacao/>
-      </View>
-    </View>
-  );
+    const limparMemoria = () => {
+        setEstado({...estadoInicial})
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.visor}>
+                <Visor valorVisor={estado.valorVisor} />
+            </View>
+            
+            <View style={styles.botoes}>
+                <Botao label="CE" triplo onClick={limparMemoria} />
+                <Botao label="/" operacao onClick={setOperacao}/>
+
+                <Botao label="7" onClick={adicionarDigito} />
+                <Botao label="8" onClick={adicionarDigito} />
+                <Botao label="9" onClick={adicionarDigito} />
+                <Botao label="*" operacao onClick={setOperacao} />
+
+                <Botao label="4" onClick={adicionarDigito}  />
+                <Botao label="5" onClick={adicionarDigito} />
+                <Botao label="6" onClick={adicionarDigito} />
+                <Botao label="-" operacao onClick={setOperacao} />
+
+                <Botao label="1" onClick={adicionarDigito}  />
+                <Botao label="2" onClick={adicionarDigito} />
+                <Botao label="3" onClick={adicionarDigito} />
+                <Botao label="+" operacao onClick={setOperacao}/>
+
+                <Botao label="0" duplo onClick={adicionarDigito} />
+                <Botao label="." onClick={adicionarDigito} />
+                <Botao label="=" operacao onClick={setOperacao}/>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  Visor:{
-    flex: 1,
-    backgroundColor: '#888'
-  },
-  botao: {
-    flexDirection: 'row',
-    flexWrap: 'wrap'
-  }
-});
+    container: {
+        flex: 1
+    },
+    visor: {
+        flex: 1,
+        backgroundColor: '#888'
+    },
+    botoes: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    }
+})
